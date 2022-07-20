6194c78c83a907ea79dbb9087f4abb2a9b823344
fe6471 <108865678+fe6471@users.noreply.github.com>
2022-07-20 17:06:49 +0900

    Update and rename leetcode_33_SearchInRotatedSortedArray to leetcode_33_SearchInRotatedSortedArray.py

--- binary search/leetcode_33_SearchInRotatedSortedArray.py
+++ binary search/leetcode_33_SearchInRotatedSortedArray.py
@@ -1,13 +1,13 @@
-Search in Rotated Sorted Array [Medium]
-https://leetcode.com/problems/search-in-rotated-sorted-array/
+# Search in Rotated Sorted Array [Medium]
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-Solution 1
-[idea]
-Find k (index of minimum value) using binary search.
-Move elements in the array k times and then find target using binary search.
+# Solution 1
+# [idea]
+# Find k (index of minimum value) using binary search.
+# Move elements in the array k times and then find target using binary search.
 
-[code]
-class Solution:
+# [code]
+class Solution1:
     def search(self, nums: List[int], target: int) -> int:
         # finding k
         low, high = 0, len(nums) - 1
@@ -42,19 +42,19 @@ class Solution:
         
         return -1
         
-time complexity = O(logn)
-space complexity = O(n)
+# time complexity = O(logn)
+# space complexity = O(n)
 
-Solution 2
-[idea]
-Don't actually need to find k.
-Let's say target is 7 with given array n = [5,6,7,8,9,10,0,1,2,3,4].
-We don't have to search in [0,1,2,3,4], so we can adjust the array like this [5,6,7,8,9,10,inf,inf,inf,inf,inf].
-Or if target is 3 We don't have to search in [5,6,7,8,9,10], so we can adjust the array like this [-inf,-inf,-inf,-inf,-inf,-inf,0,1,2,3,4]
-Note! No need to change the actual array. Just adjust the range while doing binary search.
+# Solution 2
+# [idea]
+# Don't actually need to find k.
+# Let's say target is 7 with given array n = [5,6,7,8,9,10,0,1,2,3,4].
+# We don't have to search in [0,1,2,3,4], so we can adjust the array like this [5,6,7,8,9,10,inf,inf,inf,inf,inf].
+# Or if target is 3 We don't have to search in [5,6,7,8,9,10], so we can adjust the array like this [-inf,-inf,-inf,-inf,-inf,-inf,0,1,2,3,4]
+# Note! No need to change the actual array. Just adjust the range while doing binary search.
 
-[code]
-class Solution:
+# [code]
+class Solution2:
     def search(self, nums: List[int], target: int) -> int:
         low, high = 0, len(nums)
         while low < high:
@@ -73,5 +73,5 @@ class Solution:
         
         return -1
         
-time complexity = O(logn)
-space complexity = O(1)
+# time complexity = O(logn)
+# space complexity = O(1)
