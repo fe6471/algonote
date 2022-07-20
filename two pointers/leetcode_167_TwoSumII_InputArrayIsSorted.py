6ded524e4cb83ef64d21806582bbb65f151fbca2
fe6471 <108865678+fe6471@users.noreply.github.com>
2022-07-20 17:02:16 +0900

    Update and rename leetcode_167_TwoSumII_InputArrayIsSorted to leetcode_167_TwoSumII_InputArrayIsSorted.py

--- two pointers/leetcode_167_TwoSumII_InputArrayIsSorted.py
+++ two pointers/leetcode_167_TwoSumII_InputArrayIsSorted.py
@@ -1,13 +1,13 @@
-Two Sum II - Input Array Is Sorted [Medium]
-https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+# Two Sum II - Input Array Is Sorted [Medium]
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
-Solution1
-[idea]
-Starts at idx1 = 0, idx2 = n - 1.
-If numbers[idx1] + numbers[idx2] < target, increase idx1.
-If numbers[idx1] + numbers[idx2] > target, decrease idx2.
+# Solution1
+# [idea]
+# Starts at idx1 = 0, idx2 = n - 1.
+# If numbers[idx1] + numbers[idx2] < target, increase idx1.
+# If numbers[idx1] + numbers[idx2] > target, decrease idx2.
 
-[code]
+# [code]
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         idx1, idx2 = 0, len(numbers)-1
@@ -20,5 +20,5 @@ class Solution:
         
         return [idx1 + 1, idx2 + 1]
         
-time complexity : O(n)
-space complexity : O(1)
+# time complexity : O(n)
+# space complexity : O(1)
