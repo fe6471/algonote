91f56c06df151bf7fb7e1006e153a4c74c4379af
fe6471 <108865678+fe6471@users.noreply.github.com>
2022-07-22 08:36:51 +0900

    Create leetcode_11_ContainerWithMostWater.py

---
+++ two pointers/leetcode_11_ContainerWithMostWater.py
@@ -0,0 +1,28 @@
+# Container With Most Water [Medium]
+# https://leetcode.com/problems/container-with-most-water/
+
+# Solution1
+# [idea]
+# The amount of water contained between height[i] and height[j] is (j - i) * min(height[i], height[j]).
+# To possibly increase the amount, move index of minimum height.
+
+# [code]
+class Solution1:
+    def maxArea(self, height: List[int]) -> int:
+        max_amount = 0
+        i, j = 0, len(height) - 1
+        
+        while(i < j):
+            h = min(height[i], height[j])
+            amount = h*(j-i)
+            max_amount = max(max_amount, amount)
+            
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+                
+        return max_amount
+      
+# time complexity : O(n)
+# space complexity : O(1)
