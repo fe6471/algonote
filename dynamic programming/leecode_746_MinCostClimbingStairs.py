7ac5014614919d8d851726bd3b886f481ed5cdfc
fe6471 <108865678+fe6471@users.noreply.github.com>
2022-07-22 15:07:13 +0900

    Create leecode_746_MinCostClimbingStairs.py

---
+++ dynamic programming/leecode_746_MinCostClimbingStairs.py
@@ -0,0 +1,32 @@
+# Min Cost Climbing Stairs [Easy]
+# https://leetcode.com/problems/min-cost-climbing-stairs/
+
+# Solution1
+# [idea]
+# Let's say dp[i] means minimum cost to get to top from ith position.
+# Since it can take either one step or two steps, dp[i] = min(dp[i + 1], dp[i + 2]) + cost[i]
+
+# [code]
+class Solution1:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0]*len(cost)
+        dp[-1], dp[-2] = cost[-1], cost[-2]
+        
+        for i in range(len(cost) - 3, -1, -1):
+            dp[i] = min(dp[i + 1], dp[i + 2]) + cost[i]
+        
+        return min(dp[0], dp[1])
+      
+# time complexity : O(n)
+# space complexity : O(n)
+
+# Solution2
+# [idea]
+# Same idea but don't need extra space.
+
+# [code]
+class Solution2:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
