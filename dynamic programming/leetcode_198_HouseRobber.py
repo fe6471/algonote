5976bc6b8b8c645c2b21fd0375094ac9400900cd
fe6471 <108865678+fe6471@users.noreply.github.com>
2022-07-23 22:18:10 +0900

    Create leetcode_198_HouseRobber.py

---
+++ dynamic programming/leetcode_198_HouseRobber.py
@@ -0,0 +1,21 @@
+# House Robber [Medium]
+# https://leetcode.com/problems/house-robber/
+
+# Solution1
+# [idea]
+# For ith house, it can be choosed either nums[i] + rob[0:i - 2] or rob[0:i - 1].
+
+# [code]
+class Solution1:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        
+        for num in nums:
+            tmp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = tmp
+        
+        return rob2
+      
+# time complexity : O(n)
+# space complexity : O(1)
