91ed807c26b2dd575192f876ad4a603f2e5c7751
fe6471 <108865678+fe6471@users.noreply.github.com>
2022-08-09 17:36:50 +0900

    Update leetcode_49_GroupAnagrams.py

--- array&hashing/leetcode_49_GroupAnagrams.py
+++ array&hashing/leetcode_49_GroupAnagrams.py
@@ -6,7 +6,7 @@
 # A same group of anagrams has same numbers of characters.
 
 # [code]
-class Solution1:
+class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
         
